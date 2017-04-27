b4a7dd9a1b012f1338a780edf35a3daaa7fd5d3f
Riley Hoonan <Riley.Hoonan@gmail.com>
2017-04-27 08:46:31 -0400

    crawler cleanup

--- work/crawler/collect.js
+++ work/crawler/collect.js
@@ -1,28 +1,13 @@
-// Thesis - Data Wrangling - To Do:
-
-// • Need to gather countries and remove them in pre-processing
-// • Add Dada set into final data via…? needs to be ranked - it has to come in early.
-//  	- re-scrape Dada with updated crawler,
-//      add that in p-reduce, remove that from the other stuff
-//
-// -> How does final dataset get made?
-// • Continue scrape
-//
-//-----------------------------------------------------------------------------
 //collect.js gets gets pages and runs them through the data pipeline
 
-//  Output data structure:
+//  Output schema:
 ///    this page's URL                  url        str
+///---->    the next closest root to Dada    url        [] (handle case dup)
+///    the page's title                 words      str
+///    the page's image, if it has one  url        str
 ///    links from this page             mapsTo     []
 ///    pages linking to this one        mapsFrom   []
 ///    metaData from wiki's data page   metaData   {}
-///    if object should not be scraped  noscrape   binary (either has attr or does not have it)
-
-// Will need to ID 'influencers' - things that bear an outsize influence on all events of time, such as WWI, and catalogue them
-// appropriately / define them as stopping points to keep thesis centered around Dada
-
-// RUN NOTES:
-// Wikipedia appears to block IP at some point (unclear what the cutoff is)
 
 var request = require('request');
 var fs = require('fs');
@@ -40,14 +25,12 @@ var pages = [];
 
 // test URLs - 'Francis_Picabia' - 'Ann%C3%A9es_folles' - 'Dada'
 
-// pagesIn[pgI] counter
-var pgI = 0;
-// array of urls to scrape (this will be a parameter)
-var urlArr = pagesIn[pgI].mapsFrom;
-// loop start
-var i = 0;
-// loop endPoint
-var endLoop = urlArr.length-1;
+var pgI = 0,                        // pagesIn[pgI] counter
+    urlArr = pagesIn[pgI].mapsFrom, // array of urls to scrape (this will be a parameter)
+    i = 0,                          // loop start
+    endLoop = urlArr.length-1,      // loop endPoint
+    lastBatch = 0,                  //initialize lastBatch
+    var dups = [];                  // track already scraped pages to avoid dups
 
 var exceptions = [
   'wikisource.org',
@@ -64,11 +47,6 @@ var fullSkip = [
   '_talk:',         // no talk pages
   'Draft:'
 ]
-
-
- // Empty Array that is filled with already-scraped items
- // to avoid potentially scraping the same page twice
-var dups = [];
 
 // check for exceptions
 function skip(link, exc) {
@@ -99,9 +77,6 @@ function getRando(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
-//initialize lastBatch
-var lastBatch = 0;
-
 function writeDataFile(counter) {
   counter++;
   if (counter>=endLoop || counter%250==0) { //if last loop or if counter is divisible by 250, write the file
@@ -123,11 +98,9 @@ function crawler () {           //  create a loop function
           var page = new Object;
           page.distance = 1;
           page.root = pagesIn[pgI].url; // will need to modify this for next batch?
-          page.url =  urlArr[i]; //testURL_1;
-          //track already scraped pages
+          page.url =  urlArr[i];        // testURL_1;
           dups.push(page.url);
 
-          //this scraper only handles english and is not equipped for non-english pages
           var url = 'https://en.wikipedia.org/wiki/'+page.url;
           var mapsFromURL = 'https://en.wikipedia.org/w/index.php?title=Special:WhatLinksHere/'+page.url+'&limit=3000';
 
@@ -139,7 +112,6 @@ function crawler () {           //  create a loop function
             if (err) {throw err;}
               console.log('scraping mapsFrom '+mapsFromURL);
               page.mapsFrom = mF.scrape(body);
-             //  console.log(page.mapsFrom);
              if (!skip(page.url, exceptions)) {
 
               // collect links pointing outwards from the page
@@ -147,12 +119,10 @@ function crawler () {           //  create a loop function
                  if (err) {throw err;}
                   console.log('scraping mainPage '+url);
                   var s = mT.scrape(body, url);
-                 //  console.log(s);
                   page.mapsTo = s.mapsTo;
                   page.wikiData = s.wikiData;
                   page.image = s.image;
                   page.title = s.title;
-
 
                  if (page.wikiData!=undefined) {
                    // collect wikiData (categorical info)
@@ -162,8 +132,7 @@ function crawler () {           //  create a loop function
                        page.metaData = dataScrape.scrape(body);
                        pages.push(page);
                        writeDataFile(i);
-
-                   }); // end wikiData request
+                   });
                  } else {
                    // case no wikiData
                    page.metaData = null;
@@ -171,35 +140,32 @@ function crawler () {           //  create a loop function
                    writeDataFile(i);
                  }
               }); // end mainPage request
-
             } else {
-              // some urls lead to direct sources, like images and manifesto, that do not have 'mainPage' or 'wikiData' items
-              // so just push the page as is
+              // some page types don't have link info, push as-is
               pages.push(page);
               writeDataFile(i);
             }
-
           });
-
         } else {
           page.noScrape = 1;
           pages.push(page);
           console.log('-NO SCRAPE-');
           writeDataFile(i);
         } // end fullSkip conditional
-
       } // end dups conditional
-
     //update count and check for end case
     i++;
     if (i < endLoop) { //pagesIn[pgI].mapsTo.length-1
          console.log(i+' of '+ endLoop);
          crawler();
     }
-
-    //set delay between 1200 and 3600 milliseconds per request
+  //set delay between 1200 and 3600 milliseconds per request
   }, getRando(1200,3600))
-
 }
-//
+
 crawler();
+
+// async loop
+// moves through each array item
+// updates filenames
+// clears
